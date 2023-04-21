bd2340d8701d0ed80012a265ca330a448b3cec71
Lee Vang <vanglee.20@gmail.com>
2023-04-21 15:13:45 -0500

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,8 +16,49 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+
     // Run your code here, after you have insured that the connection was made
+
+    const details =
+    {
+      title: 'Chicken',
+      level: 'Amateur Chef',
+      ingredients: ['salt', 'pepper'],
+      cuisine: 'southern',
+      dishType: 'main_course',
+      image: null,
+      duration: 3,
+      creator: 'Lee',
+      created: null,
+    }
+
+    return Recipe.create(details)
+
   })
+  .then((recipe) => {
+    console.log('Title: ', recipe.title)
+   return Recipe.insertMany(data)
+
+  })
+  .then(()=>{
+  return Recipe.findOneAndUpdate(
+    {title: "Rigatoni alla Genovese"},{ duration: 100 }, { new: true})
+  })
+
+  .then((updated) => {
+   console.log('updated recipe', updated)
+  })
+
+  .then(() => {
+    return Recipe.deleteOne({ name: 'Carrot Cake' })
+  })
+
+  .then(() => {
+    mongoose.connection.close(function () {
+      console.log('Mongoose default connection closed');
+    });
+  })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
